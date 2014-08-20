43410d18662d58d4d83a0b9ec4441685a7c66a2b
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-20 11:19:15 +0200

    Fix bug when switching to a non cobol code edit (e.g. a generic code edit)

--- pyqode/cobol/widgets/outline.py
+++ pyqode/cobol/widgets/outline.py
@@ -79,6 +79,13 @@ class OutlineTreeWidget(QtWidgets.QTreeWidget):
             self.expandAll()
             for item_to_collapse in reversed(self._to_collapse):
                 self.collapseItem(item_to_collapse)
+        else:
+            root = QtWidgets.QTreeWidgetItem()
+            root.setText(0, 'No data')
+            root.setIcon(0, QtGui.QIcon.fromTheme(
+                'emblem-important',
+                QtGui.QIcon(':/ide-icons/rc/emblem-important.png')))
+            self.addTopLevelItem(root)
         self._updating = False
 
     def set_editor(self, editor):
@@ -86,23 +93,30 @@ class OutlineTreeWidget(QtWidgets.QTreeWidget):
         :type editor: pyqode.cobol.widgets.CobolCodeEdit
         :return:
         """
-        if self._editor:
-            self._editor.outline_mode.changed.disconnect(self._update)
-            self._editor.folding_panel.trigger_state_changed.disconnect(
-                self._on_block_state_changed)
-            self._editor.folding_panel.collapse_all_triggered.disconnect(
-                self.collapseAll)
-            self._editor.folding_panel.expand_all_triggered.disconnect(
-                self.expandAll)
+        if self._editor and self._editor != editor:
+            try:
+                self._editor.outline_mode.changed.disconnect(self._update)
+                self._editor.folding_panel.trigger_state_changed.disconnect(
+                    self._on_block_state_changed)
+                self._editor.folding_panel.collapse_all_triggered.disconnect(
+                    self.collapseAll)
+                self._editor.folding_panel.expand_all_triggered.disconnect(
+                    self.expandAll)
+            except AttributeError:
+                pass
         self._editor = editor
-        self._editor.outline_mode.changed.connect(self._update)
-        self._editor.folding_panel.trigger_state_changed.connect(
-            self._on_block_state_changed)
-        self._editor.folding_panel.collapse_all_triggered.connect(
-            self.collapseAll)
-        self._editor.folding_panel.expand_all_triggered.connect(
-            self.expandAll)
-        self._update(editor.outline_mode.root_node)
+        try:
+            self._editor.outline_mode.changed.connect(self._update)
+            self._editor.folding_panel.trigger_state_changed.connect(
+                self._on_block_state_changed)
+            self._editor.folding_panel.collapse_all_triggered.connect(
+                self.collapseAll)
+            self._editor.folding_panel.expand_all_triggered.connect(
+                self.expandAll)
+        except AttributeError:
+            self._update(None)
+        else:
+            self._update(editor.outline_mode.root_node)
 
     def _convert_name(self, name, editor):
         ti = QtWidgets.QTreeWidgetItem()
@@ -120,7 +134,6 @@ class OutlineTreeWidget(QtWidgets.QTreeWidget):
 
         if TextBlockHelper.get_fold_trigger_state(name.block):
             self._to_collapse.append(ti)
-            print('to_collapsed', name.name)
 
         for child in name.children:
             ti_ch = self._convert_name(child, editor)
