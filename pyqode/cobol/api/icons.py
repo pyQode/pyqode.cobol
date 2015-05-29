7fc3c49c4152724a460a2dc909ffc07b911a95e2
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-05-29 10:45:55 +0200

    Fix icons when no theme is available (Windows/OSX)

--- pyqode/cobol/api/icons.py
+++ pyqode/cobol/api/icons.py
@@ -3,9 +3,9 @@ Store the various icon definition used thought the project.
 
 """
 ICON_COBOL = ":/icons/share/icons/pyqode.cobol/silex-192x192.png"
-ICON_DIVISION = ('code-class', ':/pyqode_python_icons/rc/class.png')
-ICON_FUNC = ('code-function', ':/pyqode_python_icons/rc/func.png')
-ICON_KEYWORD = ('quickopen', ':/pyqode_python_icons/rc/keyword.png')
+ICON_DIVISION = ('code-class', ':/icons/share/icons/pyqode.cobol/class.png')
+ICON_FUNC = ('code-function', ':/icons/share/icons/pyqode.cobol/func.png')
+ICON_KEYWORD = ('quickopen', ':/icons/share/icons/pyqode.cobol/keyword.png')
 ICON_MIMETYPE = ":/icons/share/icons/pyqode.cobol/cobol-mimetype.png"
-ICON_SECTION = ('code-context', ':/pyqode_python_icons/rc/namespace.png')
-ICON_VAR = ('code-variable', ':/pyqode_python_icons/rc/var.png')
+ICON_SECTION = ('code-context', ':/icons/share/icons/pyqode.cobol/namespace.png')
+ICON_VAR = ('code-variable', ':/icons/share/icons/pyqode.cobol/var.png')
