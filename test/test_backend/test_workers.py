6e6d3cb23db5620c60d0e1ffa01022b2a5545221
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-12-30 12:41:56 +0100

    Fix tests
    More keywords means more code completions :)

--- test/test_backend/test_workers.py
+++ test/test_backend/test_workers.py
@@ -1,9 +1,9 @@
 from pyqode.cobol.backend.workers import CobolCodeCompletionProvider
-
+from pyqode.cobol.api.keywords import INTRINSICS, RESERVED
 
 def test_code_completion():
     with open('test/testfiles/VIRTUAL-PRINTER.cbl', 'r') as f:
         code = f.read()
     provider = CobolCodeCompletionProvider()
     completions = provider.complete(code, 0, 0, '', '', '')
-    assert len(completions) == 491
+    assert len(completions) >= len(INTRINSICS + RESERVED)
